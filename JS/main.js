b00b519ce7f2ff640d0bd59e9b6b2cdafdacb858
udeilton de sousa <deilton_black@hotmail.com>
2024-09-18 09:15:24 -0300

    part6

--- JS/main.js
+++ JS/main.js
@@ -9,7 +9,7 @@ function onChangePassword() {
 }
 
 function isEmailValid() {
-  const email = document.getElementById("email").value;
+  const email = form.email().value;
   if (!email) {
     return false;
   }
@@ -17,52 +17,42 @@ function isEmailValid() {
 }
 
 function toggleEmailErrors() {
-  const email =  document.getElementById('email').value;
-  if (!email) {
-    document.getElementById('email-required-error').style.display = "block";
-  } else {
-    document.getElementById('email-required-error').style.display = "none";
-  }
+  const email = form.email().value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
 
-  if (validateEmail(email)) {
-    document.getElementById('email-invalid-error').style.display = "none";
-  } else {
-    document.getElementById('email-invalid-error').style.display = "block";
-  }
-
+  form.emaiInvalidError().style.display = validateEmail(email) ? "none" : "block";
 }
 
 function togglePasswordError() {
-  const password = document.getElementById('password').value;
-  if (!password) {
-    document.getElementById('password-required-error').style.display = "block"; 
-  } else{
-    document.getElementById('password-required-error').style.display = "none"; 
-  }
+  const password = form.password().value;
+
+  form.passwordRequiredError().style.display = password ? "none" : "block";
 }
 
 function toggleButtonsDisable() {
   const emailValid = isEmailValid();
-  document.getElementById("recover-password-button").disabled = !emailValid;
+  form.recoverPassword().disabled = !emailValid;
 
   const passwordValid = isPasswordValid();
-  document.getElementById('login-botton').disabled = !emailValid || !passwordValid;
+  form.loginButton().disabled = !emailValid || !passwordValid;
 
 }
 
 function isPasswordValid() {
-  const password = document.getElementById("password").value;
+  const password = form.password().value;
   if (!password) {
     return false;
   }
   return true;
 }
-function validateEmail(email) {
-  return /\S+@\S+\.\S+/.test(email);
-}
 
-// pegar o valor do campo de email
-// verifica se  o email não é vazío e se é valido
-// se verdadeiro, então habilitar o botão de recuperar senha
-// se falto, então dessabilitar o botão de recuperar senha
+const form = {
+  email: () => document.getElementById('email'),
+  emaiInvalidError: () => document.getElementById('email-invalid-error'),
+  emailRequiredError: () => document.getElementById('email-required-error'),
+  loginButton: () => document.getElementById('login-botton'),
+  password: () => document.getElementById('password'),
+  passwordRequiredError: () => document.getElementById('password-required-error'),
+  recoverPassword: () => document.getElementById('recover-password-button'),
+}
 
---
+++ JS/validations.js
@@ -0,0 +1,4 @@
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+  
